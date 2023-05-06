cd021d3b0ccc80257e308ceb32899091f45b7944
tomoki-yamamura <progtab17@gmail.com>
2023-05-06 15:52:41 +0900

    merge sort solutino

--- linked_list/23/marge_k_sorted_lists.go
+++ linked_list/23/marge_k_sorted_lists.go
@@ -5,35 +5,81 @@ type ListNode struct {
     Next *ListNode
 }
  
+// my first solutin: OK
+// Beats 14.76%  Beats 53.2%
+
+// func mergeKLists(lists []*ListNode) *ListNode {
+// 	listsLength := len(lists)
+// 	if listsLength == 0 {
+// 		return nil
+// 	}
+
+// 	if listsLength == 1 {
+// 		return lists[0]
+// 	}
+
+// 	prev := lists[0]
+// 	for i := 1; i < listsLength; i++ {
+// 		current := lists[i]
+// 		prev = mergeTwoLists(prev, current)
+// 	}
+// 	return prev
+// }
+
+// func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+// 	if list1 == nil {
+// 		return list2
+// 	}
+// 	if list2 == nil {
+// 		return list1
+// 	}
+// 	if list1.Val < list2.Val {
+// 		list1.Next = mergeTwoLists(list1.Next, list2)
+// 		return list1
+// 	}
+// 	list2.Next = mergeTwoLists(list1, list2.Next)
+// 	return list2
+// }
+
+// Best Solution
+// Beats 100%  Beats 97.63%
+// this is mergeSortLogic
+
 func mergeKLists(lists []*ListNode) *ListNode {
-	listsLength := len(lists)
-	if listsLength == 0 {
-		return nil
+	if len(lists) == 0 {
+			return nil
 	}
-
-	if listsLength == 1 {
-		return lists[0]
+	
+	k := len(lists)
+	if k == 1 {
+			return lists[0]
 	}
-
-	prev := lists[0]
-	for i := 1; i < listsLength; i++ {
-		current := lists[i]
-		prev = mergeTwoLists(prev, current)
-	}
-	return prev
+	
+	l1 := mergeKLists(lists[:k/2])
+	l2 := mergeKLists(lists[k/2:])
+	return mergeTwoLists(l1, l2)
 }
 
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	if list1 == nil {
-		return list2
+func mergeTwoLists(l1, l2 *ListNode) *ListNode {
+	var dummyHead ListNode
+	
+	cur := &dummyHead
+	for l1 != nil && l2 != nil {
+			if l1.Val < l2.Val {
+					cur.Next = l1
+					l1 = l1.Next
+			} else {
+					cur.Next = l2
+					l2 = l2.Next
+			}
+			cur = cur.Next
 	}
-	if list2 == nil {
-		return list1
+	
+	if l1 != nil {
+			cur.Next = l1
+	} else if l2 != nil {
+			cur.Next = l2
 	}
-	if list1.Val < list2.Val {
-		list1.Next = mergeTwoLists(list1.Next, list2)
-		return list1
-	}
-	list2.Next = mergeTwoLists(list1, list2.Next)
-	return list2
+	
+	return dummyHead.Next
 }
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	lists := []int{1,2,3,4}
+	length := len(lists)
+	fmt.Println(lists[0:length/3])
+	fmt.Println(length/3)
 }
