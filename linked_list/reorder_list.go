32e35be3c2804db1987bba6d439b5453e16fe8cb
tomoki-yamamura <progtab17@gmail.com>
2023-05-07 16:57:57 +0900

    pra pointer

---
+++ linked_list/reorder_list.go
@@ -0,0 +1,64 @@
+package link_list
+
+// i couldnt solve
+// func reorderList(head *ListNode)  {
+// 	reversed := reverse(head)
+// 	mergeTwoList(head, reversed)
+// }
+
+// func reverse(head *ListNode) (prev *ListNode) {
+// 	for head != nil {
+// 		tmp := head.Next
+// 		head.Next = prev
+// 		prev = head
+// 		head = tmp
+// 	}
+// 	return
+// }
+
+// func mergeTwoList(head *ListNode, reversed *ListNode) {
+// 	tmpHead := head
+// 	tmpReversed := reversed
+// 	var counter int
+// 	for tmpHead != nil {
+// 		counter++
+// 		if counter % 2 == 0 {
+// 			//偶数
+// 			reversed
+// 		} else {
+// 			// 奇数
+// 			tmp := head.Next
+// 			head.Next = tmpReversed
+// 			head = tmp
+// 		}
+// 	}
+// }
+
+func ReorderList(head *ListNode) {
+	// find midpoint
+	fast := head
+	mid := head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		mid = mid.Next
+	}
+
+	var prev *ListNode
+
+	for mid != nil {
+		temp := mid.Next
+		mid.Next = prev
+		prev = mid
+		mid = temp
+	}
+
+	start := head
+
+	for prev.Next != nil {
+		tmp1, tmp2 := start.Next, prev.Next
+		start.Next = prev
+		prev.Next = tmp1
+		start, prev = tmp1, tmp2
+	}
+}
--- main.go
+++ main.go
@@ -1,33 +1,62 @@
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/tomoki-yamamura/go-docker/linked_list"
+// )
+
+// func main() {
+// 	// arr := &link_list.ListNode{
+// 	// 	Val: 1,
+// 	// 	Next: &link_list.ListNode{
+// 	// 		Val: 2,
+// 	// 		Next: &link_list.ListNode{
+// 	// 			Val: 3,
+// 	// 			Next: nil,
+// 	// 		},
+// 	// 	},
+// 	// }
+// 	arr := &link_list.ListNode{
+// 		Val: 1,
+// 		Next: &link_list.ListNode{
+// 			Val: 2,
+// 			Next: &link_list.ListNode{
+// 				Val: 3,
+// 				Next: &link_list.ListNode{
+// 					Val: 4,
+// 					Next: nil,
+// 				},
+// 			},
+// 		},
+// 	}
+
+// 	link_list.ReorderList(arr)
+// 	fmt.Println(arr)
+// }
+
 package main
 
-import (
-	"github.com/tomoki-yamamura/go-docker/linked_list"
-)
+import "fmt"
+
+type Person struct {
+	Name string
+	Age int
+}
 
 func main() {
-	arr := &link_list.ListNode{
-		Val: 1,
-		Next: &link_list.ListNode{
-			Val: 2,
-			Next: &link_list.ListNode{
-				Val: 3,
-				Next: nil,
-			},
-		},
-	}
-	// arr := &link_list.ListNode{
-	// 	Val: 1,
-	// 	Next: &link_list.ListNode{
-	// 		Val: 2,
-	// 		Next: &link_list.ListNode{
-	// 			Val: 3,
-	// 			Next: &link_list.ListNode{
-	// 				Val: 4,
-	// 				Next: nil,
-	// 			},
-	// 		},
-	// 	},
-	// }
+	p := &Person{Name: "Bob", Age: 30}
+	fmt.Printf("before: %p\n", p) // &{Bob 30}
+	updatePersonPointer(p)
+	fmt.Printf("after: %p\n", p) // &{Bob 30}
 
-	link_list.RemoveNthFromEnd(arr, 1)
+	fmt.Println(p)
+}
+
+func updatePersonPointer(p *Person) {
+	fmt.Printf("arg P: %p\n", p)
+	newP := &Person{Name: "Charlie", Age: 35}
+	fmt.Printf("newP: %p\n", newP)
+	p = newP
+	fmt.Printf("p: %p, newP: %p\n", p, newP)
 }
