5ef0e10af3a4ac90bb1c0d07d89389c34e4e2607
tomoki-yamamura <progtab17@gmail.com>
2023-05-13 00:20:37 +0900

    except array

--- array/product_except_self.go
+++ array/product_except_self.go
@@ -2,7 +2,23 @@ package array
 
 
 // couldnt
-func productExceptSelf(nums []int) []int {
+// func productExceptSelf(nums []int) []int {
   
-}
+// }
 
+// example
+func productExceptSelf(nums []int) []int {
+	result := make([]int, len(nums))
+	pre := 1
+	for i := 0; i < len(nums); i++ {
+		result[i] = pre
+		pre = pre * nums[i]
+	}
+	pre = 1
+	for i := len(nums)-1; i >= 0; i-- {
+		result[i] *= pre
+		pre = pre * nums[i]
+	}
+
+	return result
+}
