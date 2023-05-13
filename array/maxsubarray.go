93a7799caf2d143a505b9f2a486d56a24214abe4
tomoki-yamamura <progtab17@gmail.com>
2023-05-14 03:06:34 +0900

    kadanes

--- array/maxsubarray.go
+++ array/maxsubarray.go
@@ -1,1 +1,21 @@
 package array
+
+func maxSubArray(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	tmp := nums[0]
+	result := nums[0]
+	for i := 1; i < len(nums); i++ {
+		tmp = max(nums[i], tmp+nums[i]) // -1
+		result = max(result, tmp) // 1
+	}
+	return tmp
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} 
+	return b
+}
